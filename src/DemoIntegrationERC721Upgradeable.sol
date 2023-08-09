4de73313cf6ee9ef5d8aabaa6470946ffea3abbb
Craigson <craigpick@gmail.com>
2023-08-09 09:30:11 -0400

    include tests for upgrading the implementation

--- src/DemoIntegrationERC721Upgradeable.sol
+++ src/DemoIntegrationERC721Upgradeable.sol
@@ -12,7 +12,7 @@ import {Cube3IntegrationUpgradeable} from "cube3/contracts/upgradeable/Cube3Inte
 
 // The `Initializable` base contract is inherited by SecurityAdmin2StepUpgradeable, which is inherited by Cube3IntegrationUpgradeable, which is inherited by
 // Cube3IntegrationERC721Upgradeable. Therefore, we don't import `Initializable` here to avoid a linearization of the inheritance graph error.
-contract DemoIntegrationERC721Upgradeable is Cube3IntegrationUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract DemoIntegrationERC721UpgradeableNoModifier is Cube3IntegrationUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
@@ -37,8 +37,78 @@ contract DemoIntegrationERC721Upgradeable is Cube3IntegrationUpgradeable, ERC721
         __Cube3IntegrationUpgradeable_init(securityAdmin);
     }
 
-    // Add the cube3Protected modifier to the safeMint function, and include the `bytes calldata cube3SecurePayload` parameter as
-    // the last parameter of the function. This will allow the Cube3Integration contract to validate the payload before the mint.
+    // the `safeMint` fn intentionally omits the `cube3Protected` modifier
+    function safeMint(uint256 qty) public {
+        require(mintsPerAddress[msg.sender] + qty <= MAX_MINT, "Max mint per address reached");
+        mintsPerAddress[msg.sender] += qty;
+        
+        uint256 tokenId;
+        for (uint i; i < qty; ) {
+            tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+
+            _safeMint(msg.sender, tokenId);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner // could also use `onlySecurityAdmin` if the security admin is the same account as the deployer
+        override
+    {
+        // preAuthorize the new implementation with the CUBE3 protocol
+        _preAuthorizeNewImplementation(newImplementation);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // We need to override `supportsInterface` for all the upgradeable contracts we inherit from that
+    // support ERC165.
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(Cube3IntegrationUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
+contract DemoIntegrationERC721UpgradeableWithModifier is Cube3IntegrationUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    mapping(address => uint256) public mintsPerAddress;
+
+    uint256 constant public MAX_MINT = 3;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address securityAdmin) initializer public {
+        // initialize the token contract
+        __ERC721_init("MyToken", "MTK");
+        __ERC721Enumerable_init();
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        // initialize the CUBE3 integration
+        __Cube3IntegrationUpgradeable_init(securityAdmin);
+    }
+
     function safeMint(uint256 qty, bytes calldata cube3SecurePayload) public cube3Protected(cube3SecurePayload) {
         require(mintsPerAddress[msg.sender] + qty <= MAX_MINT, "Max mint per address reached");
         mintsPerAddress[msg.sender] += qty;
--- test/DemoTest.t.sol
+++ test/DemoTest.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {DemoIntegrationERC721} from "../src/DemoIntegrationERC721.sol";
-import {DemoIntegrationERC721Upgradeable} from "../src/DemoIntegrationERC721Upgradeable.sol";
+import {DemoIntegrationERC721UpgradeableNoModifier,DemoIntegrationERC721UpgradeableWithModifier} from "../src/DemoIntegrationERC721Upgradeable.sol";
 import {Cube3ProtocolTestUtils} from "cube3/test/foundry/utils/deploy.sol";
 
 
@@ -12,8 +12,8 @@ contract DemoTest is Cube3ProtocolTestUtils {
     DemoIntegrationERC721 internal demo;
 
     ERC1967Proxy internal demoProxy;
-    DemoIntegrationERC721Upgradeable internal demoUpgradeable;
-    DemoIntegrationERC721Upgradeable internal wrappedDemoProxy;
+    DemoIntegrationERC721UpgradeableNoModifier internal demoUpgradeableNoModifier;
+    DemoIntegrationERC721UpgradeableNoModifier internal wrappedDemoProxy;
 
     address internal integrationSecurityAdmin = makeAddr("integrationSecurityAdmin");
 
@@ -27,22 +27,23 @@ contract DemoTest is Cube3ProtocolTestUtils {
         demo = new DemoIntegrationERC721();
 
         // deploy the upgradeable integration implementation
-        demoUpgradeable = new DemoIntegrationERC721Upgradeable();
+        demoUpgradeableNoModifier = new DemoIntegrationERC721UpgradeableNoModifier();
 
         // deploy the proxy and set the security admin address explicityl
         demoProxy = new ERC1967Proxy(
-            address(demoUpgradeable),
-            abi.encodeCall(DemoIntegrationERC721Upgradeable.initialize, (integrationSecurityAdmin))
+            address(demoUpgradeableNoModifier),
+            abi.encodeCall(DemoIntegrationERC721UpgradeableNoModifier.initialize, (integrationSecurityAdmin))
         );
 
         vm.stopPrank();
 
         // wrap the proxy in the implementation's interface for convenience
-        wrappedDemoProxy = DemoIntegrationERC721Upgradeable(address(demoProxy));
+        wrappedDemoProxy = DemoIntegrationERC721UpgradeableNoModifier(address(demoProxy));
     }
 
 
-    function testStandalone() public {
+    // test calling the `safeMint` function on a standalone integration that has function-level protecion enabled
+    function testStandaloneMint() public {
 
         // Register the the integration with the CUBE3 protocol.
         bytes4[] memory enabledByDefaultFnSelectors = new bytes4[](1);
@@ -67,10 +68,70 @@ contract DemoTest is Cube3ProtocolTestUtils {
         address user = makeAddr("user");
         vm.startPrank(user);
         demo.safeMint(1, cube3SecurePayload);
+
+
     }
 
-    function testUpgradeable() public {
 
+
+    // test calling the `safeMint` function on an upgradeable integration that has not included the `cube3Protected` modifier
+    // and successfully mints a token.
+    function testUpgradeableMintNoModifier() public {
+
+        _registerProxyWithCube3();
+
+        // call the mint function as the end-user
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        wrappedDemoProxy.safeMint(1);
+    }
+
+    // test upgrading the upgradeable ERC721 proxy to a new implementation that includes the
+    // `cube3protected` modifier on the `safeMint` function, enables function-level protection, and
+    // successfully mints a token.
+    function testUpgradeAndMintWithModifier() public {
+
+        _registerProxyWithCube3();
+        
+        vm.startPrank(integrationSecurityAdmin);
+        
+        // deploy the new implementation
+        DemoIntegrationERC721UpgradeableWithModifier demoUpgradeableWithModifier = new DemoIntegrationERC721UpgradeableWithModifier();
+
+        // upgrade the proxy to the new implementation
+        wrappedDemoProxy.upgradeTo(address(demoUpgradeableWithModifier));
+
+        // wrap the proxy in the implementation's interface for convenience
+        DemoIntegrationERC721UpgradeableWithModifier wrappedDemoProxyWithModifier = DemoIntegrationERC721UpgradeableWithModifier(address(demoProxy));
+
+        // The proxy is already registered and the new implementation is pre-authorized, however we will need to enable function
+        // protection for `safeMint` as the function selector has changed with the inclusion of the `cube3SecurePayload`
+        bytes4[] memory enabledFnSelectors = new bytes4[](1);
+        bool[] memory fnSelectorStatus = new bool[](1);
+        enabledFnSelectors[0] = wrappedDemoProxyWithModifier.safeMint.selector;
+        fnSelectorStatus[0] = true;
+        wrappedDemoProxyWithModifier.setFunctionProtectionStatus(enabledFnSelectors, fnSelectorStatus);
+
+        vm.stopPrank();
+
+        // call the mint function as the end-user
+
+        // The `cube3SecurePayload` parameter is required for all functions that are protected by the `cube3Protected` modifier.
+        // The payload is generated off-chain by the CUBE3 platform and provided to the EOA executing the TX.
+        // For the purposes of testing, we can just pass an empty bytes array, with minimum length of 64 bytes, as the payload to mimic
+        // the protocol's behaviour.
+        bytes memory cube3SecurePayload = new bytes(64);
+        
+        // call the mint function as the end-user
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        wrappedDemoProxyWithModifier.safeMint(1, cube3SecurePayload);
+
+    }
+
+    // Helper functions
+    function _registerProxyWithCube3() internal {
+    
         // Register the the integration with the CUBE3 protocol.
         bytes4[] memory enabledByDefaultFnSelectors = new bytes4[](1);
         enabledByDefaultFnSelectors[0] = wrappedDemoProxy.safeMint.selector;
